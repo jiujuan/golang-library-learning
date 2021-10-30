3c03accae44c73832998f6bbd569711ff4e6eab4
jiujuan <jiujuanfeng@163.com>
2021-10-30 19:33:02 +0800

    demo2

--- cobra/firstappname/cmd/demo2.go
+++ cobra/firstappname/cmd/demo2.go
@@ -21,41 +21,33 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// 定义 local flag
-var dsn string
-
-// demoCmd represents the demo command
-var demoCmd = &cobra.Command{
-	Use:   "demo",
+// demo2Cmd represents the demo2 command
+var demo2Cmd = &cobra.Command{
+	Use:   "demo2",
 	Short: "A brief description of your command",
-	Long: `(demo)A longer description that spans multiple lines and likely contains examples
+	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("demo called")
-		fmt.Println("cmd demo")
-		// 打印输出 name
-		fmt.Println("print persistent flag name: ", name)
-		// 打印输出local flag: dsn
-		fmt.Println("(local flag)print dsn: ", dsn)
+		fmt.Println("demo2 called")
+		// 添加输出 dsn
+		fmt.Println("test get local flag(dsn): ", dsn)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(demoCmd)
+	rootCmd.AddCommand(demo2Cmd)
 
 	// Here you will define your flags and configuration settings.
 
-	demoCmd.Flags().StringVarP(&dsn, "dsn", "d", "false", "dsn file")
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// demoCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// demo2Cmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// demoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// demo2Cmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
